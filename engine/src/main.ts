d9d76e5b66d697485eafa68f234177d54914185c
Esteban Vaca C. <estebvac@gmail.com>
2026-09-24 09:07:46 +0200

    fix: match official OrbitControls camera FOV

--- engine/src/main.ts
+++ engine/src/main.ts
@@ -515,7 +515,7 @@ class PointCloudVisualizer {
     }
 
     this.camera = new THREE.PerspectiveCamera(
-      75,
+      60,
       container.clientWidth / container.clientHeight,
       0.001,
       1000000 // Further increased far plane for disparity files
